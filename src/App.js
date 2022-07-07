bc96c89bde7e1ba419f4c90e2816be4d4d82e89c
whanye <emcallister98@gmail.com>
2022-07-07 15:14:34 -0400

    3 files modified; minimal working flip game

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SingleCard from './components/SingleCard'
 import './App.css';
 
 import cover from 'https://github.com/iamshaunjp/React-Firebase/blob/lesson-58/magic-memory/public/img/cover.png'
 
 const cardImages = [
-  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/helmet-1.png"},
-  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/potion-1.png"},
-  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/ring-1.png"},
-  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/scroll-1.png"},
-  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/shield-1.png"},
-  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/sword-1.png"}
+  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/helmet-1.png", matched: false},
+  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/potion-1.png", matched: false},
+  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/ring-1.png", matched: false},
+  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/scroll-1.png", matched: false},
+  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/shield-1.png", matched: false},
+  { "src": "https://raw.githubusercontent.com/iamshaunjp/React-Firebase/lesson-58/magic-memory/public/img/sword-1.png", matched: false}
 ]
 
 export default function App() {
@@ -38,6 +38,33 @@ export default function App() {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
 
+  //compare 2 selected cards
+  useEffect(() => {
+    if ( choiceOne && choiceTwo ) {
+      if ( choiceOne.src === choiceTwo.src ) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -46,8 +73,9 @@ export default function App() {
         {cards.map(card => 
           <SingleCard 
             card={card} 
-            key={card.id} 
+            key={card.id}
             handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched }
           />
         )}
       </div>
